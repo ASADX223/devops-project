7fd97d2f2276b13e6a4219fbcffd47bd3ae8482b
Suhasini Nanaware <suhasininanware19@gmail.com>
2026-04-16 22:36:56 +0530

    updated

--- OneDrive/Desktop/DeVops/devops-project-main-ASAD/devops-project-main/app.js
+++ OneDrive/Desktop/DeVops/devops-project-main-ASAD/devops-project-main/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps Project Running 🚀");
+  res.send("The DevOps Project is Running 🚀");
 });
+
+
 
 app.listen(3000);
 
